d21a5f563b21fcdb163c271e3b299fa204e3d6b1
Jamie Pittock <jamiepittock@me.com>
2014-04-24 11:48:13 +0100

    fixed db credentials

--- craft/config/db.php
+++ craft/config/db.php
@@ -9,26 +9,6 @@
 
 return array(
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The database username to connect with.
-	'user' => 'root',
-
-	// The database password to connect with.
-	'password' => 'root',
-
-	// The name of the database to select.
-	'database' => 'supertone_supertone_local',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
-
-);
-
-
-return array(
-
     '*' => array(
         'tablePrefix' => 'craft',
     ),
